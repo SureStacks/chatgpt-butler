d427c58e7eed7f330da01953b3296529c6b3c1b5
Cedric Blomart <cblomart@gmail.com>
2024-01-21 12:25:24 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# chatgpt-buttler
-A github action to handle incendent first response with ChatGPT
+# ChatGPT Buttler
+
+A GitHub action to handle issues first response with ChatGPT
+
+The action will reply to unanswered issues with the reply of ChatGPT. 
+
+It is highly encouraged to customize the prompt to give a bit of context.
+
+The issues will be handled as ordered by date from most recent to least.
+
+A limit of 10 issues are treated at a time.
+
+## Inputs
+
+Name | Description | Default 
+-- | --- | ---
+repo-token | token to access github | ```${{ github.token }}```
+max-issues | Maximum number of issues to process | 10
+only-labels | Only check issues with these labels | 
+prompt-preamble | Preamble to to prompt to chatgpt followed by the issue body' | 'Please help me answer the follwing github issue:\n\n'
+openai-key* | OpenAI API key | |
+chagpt-model | ChatGPT model to use | gpt-4
+delay | Delay between two requests to the OpenAI API | 5 seconds
+
+*: mandatory
+
+## Usage
+
+The task below will add the answer from ChatGPT to every unanswered issue.
+
+```yaml
+uses: surestacks/chatgpt-buttler@v0.1.0
+with:
+    openai-key: <openai key>
+```
+
+It can be limited to issues with selected labels.
+
+```yaml
+uses: surestacks/chatgpt-buttler@v0.1.0
+with:
+    only-labels: enhancement, question
+    openai-key: <openai key>
+```
---
+++ src/index.js
@@ -0,0 +1,43 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+import { ChatGPTAPI } from 'chatgpt'
+
+
+try {
+    // get octokit
+    const octokit = github.getOctokit(core.getInput('github-token'));
+    // get chatgpt api with model from 'chatgpt-model' the key is available in 'openai-key'
+    const chatgpt = new ChatGPTAPI({
+        apiKey: core.getInput('openai-key'),
+        completionParams: {
+            model: core.getInput('chatgpt-model')
+        }
+    });
+    // get unanswered issues from repository with provided labels or all if not provided
+    const issues = octokit.issues.listForRepo({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        labels: core.getInput('labels').replace(' ','').split(','),
+        state: 'open',
+        filter: 'unanswered'
+    });
+    // order the issues per creation date descending and take the x first ones. x being the provided 'max-issues'
+    const orderedIssues = issues.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)).slice(0, core.getInput('max-issues'));
+    // for each issue
+    orderedIssues.forEach(async issue => {
+        // create the prompt by prefixing the issue body with the provided 'prompt-preamble'
+        const prompt = core.getInput('prompt-preamble') + issue.body;
+        // ask chatgtp for a response to the prompt (the openai key is provided in 'openai-key')
+        await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            issue_number: issue.number,
+            body: await chatgpt(prompt, core.getInput('openai-key'))
+        });
+        // wait a bit before handling the next issue
+        await new Promise(resolve => setTimeout(resolve, core.getInput('openai-key') * 1000));
+    }
+    );
+} catch (error) {
+    core.setFailed(error.message);
+}
